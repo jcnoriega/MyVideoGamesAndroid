e428f648f2c15a6f440831d3cbf97e48a7f20da7
josenoriega <hangover230790>
2023-06-01 00:21:18 -0300

    Added epoxy to project

---
+++ app/src/main/java/com/example/myvideogames/ui/home/EpoxyDataBindingPatterns.kt
@@ -0,0 +1,7 @@
+package com.example.myvideogames.ui.home
+
+import com.airbnb.epoxy.EpoxyDataBindingPattern
+import com.example.myvideogames.R
+
+@EpoxyDataBindingPattern(rClass = R::class, layoutPrefix = "home")
+object EpoxyDataBindingPatterns
--- app/src/main/java/com/example/myvideogames/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myvideogames/ui/home/HomeFragment.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import com.airbnb.epoxy.EpoxyRecyclerView
 import com.example.myvideogames.databinding.FragmentHomeBinding
+import com.example.myvideogames.simpleItem
 
 class HomeFragment : Fragment() {
 
@@ -23,14 +26,17 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val root: EpoxyRecyclerView = binding.root
+        root.withModels {
+            for (i in 1 until 11) {
+                simpleItem {
+                    id("item{$i}")
+                    text("Number $i")
+                }
+            }
         }
         return root
     }
