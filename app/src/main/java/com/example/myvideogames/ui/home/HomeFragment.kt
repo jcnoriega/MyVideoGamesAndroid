2cad7ec3cc665e2d8e70377fed3506a02b6f5e55
josenoriega <hangover230790>
2023-06-01 15:44:56 -0300

    Add header view

--- app/src/main/java/com/example/myvideogames/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myvideogames/ui/home/HomeFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView.OnScrollListener
 import com.airbnb.epoxy.EpoxyRecyclerView
 import com.example.myvideogames.databinding.FragmentHomeBinding
+import com.example.myvideogames.header
 import com.example.myvideogames.simpleItem
 
 class HomeFragment : Fragment() {
@@ -31,6 +34,13 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: EpoxyRecyclerView = binding.root
         root.withModels {
+            header {
+                id("header")
+                title("Grand Theft Auto V")
+                onClick { _ ->
+                    Toast.makeText(requireContext(), "Header", Toast.LENGTH_SHORT).show()
+                }
+            }
             for (i in 1 until 11) {
                 simpleItem {
                     id("item{$i}")
